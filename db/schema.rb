f293b1f5b8aaa128f5309b651bb8658fe18b227d
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-01 15:52:03 -0300

    chore: run database migrations for Montagem scaffold
    - Apply migrations to create Montagem table with reference to Livro in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_185009) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_185117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_185009) do
     t.index ["autor_id"], name: "index_livros_on_autor_id"
   end
 
+  create_table "montagems", force: :cascade do |t|
+    t.bigint "livro_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["livro_id"], name: "index_montagems_on_livro_id"
+  end
+
   create_table "pecas", force: :cascade do |t|
     t.string "nome"
     t.decimal "valor"
@@ -48,5 +55,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_185009) do
   end
 
   add_foreign_key "livros", "autors"
+  add_foreign_key "montagems", "livros"
   add_foreign_key "pecas", "fornecedors"
 end
