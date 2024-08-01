bf1b5df90106c5534c16903cdc78bc819bdb3d46
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-01 16:12:29 -0300

    feat: update root route to livro#index
    - Set root route to livro#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
+  root "livros#index"
+
   resources :montagems
   resources :pecas
   resources :fornecedors
   resources :livros
   resources :autors
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
