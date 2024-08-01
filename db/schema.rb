7ea7494f6d1dd632e7b601087a7cd39b6469ea5f
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-01 15:51:04 -0300

    chore: run database migrations for Peca scaffold
    - Apply migrations to create Peca table with reference to Fornecedor in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_184730) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_185009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,5 +38,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_184730) do
     t.index ["autor_id"], name: "index_livros_on_autor_id"
   end
 
+  create_table "pecas", force: :cascade do |t|
+    t.string "nome"
+    t.decimal "valor"
+    t.bigint "fornecedor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fornecedor_id"], name: "index_pecas_on_fornecedor_id"
+  end
+
   add_foreign_key "livros", "autors"
+  add_foreign_key "pecas", "fornecedors"
 end
